ef8d2a5d3c2b61f91dfa3d1610853aa2091cc0fa
Pedro Vinicius <pedrorohadias1001@gmail.com>
2025-12-14 19:08:03 -0300

    BaseOfProject

---
+++ src/main/java/viniciusDias1001/github/com/SmartFlowGutsGym/SmartFlowGutsGymApplication.java
@@ -0,0 +1,13 @@
+package viniciusDias1001.github.com.SmartFlowGutsGym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartFlowGutsGymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartFlowGutsGymApplication.class, args);
+	}
+
+}
---
+++ src/test/java/viniciusDias1001/github/com/SmartFlowGutsGym/SmartFlowGutsGymApplicationTests.java
@@ -0,0 +1,13 @@
+package viniciusDias1001.github.com.SmartFlowGutsGym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartFlowGutsGymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
